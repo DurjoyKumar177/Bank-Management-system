8b80d7c13d127c04176f5269abc58b715a18233f
DurjoyKumar177 <durjoykumar177@gmail.com>
2024-11-15 23:34:11 +0600

    add password change option

--- amar_bank/accounts/views.py
+++ amar_bank/accounts/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render,redirect
 from django.views.generic import FormView
 from . forms import UserRegistrationForm, UserUpdateForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, update_session_auth_hash
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views import View
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
 
 # Create your views here.
+def send_transaction_email(user_account, subject, template):
+    
+    user = user_account
+    message = render_to_string(template, {
+        'user': user,
+    })
+    send_email = EmailMultiAlternatives(subject, '', to=[user.email])
+    send_email.attach_alternative(message, "text/html")
+    send_email.send()
 
 class UserRegistrationView(FormView):
     template_name = 'accounts/user_registration.html'
@@ -43,3 +58,20 @@ class UserBankAccountUpdateView(View):
             form.save()
             return redirect('profile')  # Redirect to the user's profile page
         return render(request, self.template_name, {'form': form})
+    
+class PassChangeView(LoginRequiredMixin, View):
+    def get(self, request):
+        form = PasswordChangeForm(user=request.user)
+        return render(request, 'accounts/passchange.html', {'form': form})
+
+    def post(self, request):
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)  # Prevents log out after password change
+            messages.success(request, 'Password changed successfully.')
+            
+            send_transaction_email(request.user, 'Password Change Message', 'accounts/password_change_email.html')
+            
+            return redirect('profile')  # Redirect to the profile page
+        return render(request, 'accounts/passchange.html', {'form': form})
