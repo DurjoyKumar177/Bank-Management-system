db2b3e821f3a376b51790e1f313797dd52a18bd1
DurjoyKumar177 <durjoykumar177@gmail.com>
2024-11-17 11:39:32 +0600

    link added

--- amar_bank/amar_bank/settings.py
+++ amar_bank/amar_bank/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://bank-management-system-yqrb.onrender.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
